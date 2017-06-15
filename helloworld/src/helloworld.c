ca654768ad8879408869b1f1e2c3b17d6d7f929a
Administrator <Administrator@XB-20170405RMKL>
2017-06-15 19:37:32 +0800

    C语言helloworld

---
+++ helloworld/src/helloworld.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : zhangliang
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "helloworld3.h"
+
+void main() {
+
+	printf("ÄãºÃ_01========================\n");
+	test1();
+	test();
+}
---
+++ helloworld/src/helloworld2.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : zhangliang
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void test() {
+
+	printf("\nÄãºÃ_02========================");
+
+}
